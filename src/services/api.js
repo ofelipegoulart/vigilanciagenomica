1cc48b6713b0b0964c313a931bd31902d2f59e75
Felipe Goulart <ofelipegoulart@gmail.com>
2021-11-28 12:55:22 -0300

    api update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://vigilanciagenomica-backend.herokuapp.com/"
+  baseURL: "https://vigilanciagenomica-backend.herokuapp.com/api"
 });
 
 export default api;
