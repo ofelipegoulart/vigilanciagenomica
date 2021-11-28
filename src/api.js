93e087e34b445ebd0f2737cab446ae057c72bf17
Felipe Goulart <ofelipegoulart@gmail.com>
2021-11-28 15:17:50 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,10 @@ require('dotenv').config()
 const baseURL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
-  baseURL: baseURL
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
