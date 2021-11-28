5ee8d6c4bcf5000d868f7a94e5db172307f2fcb0
Felipe Goulart <ofelipegoulart@gmail.com>
2021-11-28 16:56:58 -0300

    api update

--- src/api.js
+++ src/api.js
@@ -1,21 +1,5 @@
 import axios from "axios";
-import jwt_decode from 'jwt-decode';
 
 const axiosJWT = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
-
-axiosJWT.interceptors.request.use(
-  async (config) => {
-    let currentDate = new Date();
-    const decodedToken = jwt_decode(user.accessToken);
-    if (decodedToken.exp * 1000 < currentDate.getTime()) {
-      data = await refreshToken();
-      config.headers["authorization"] = "Bearer " + data.accessToken;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
