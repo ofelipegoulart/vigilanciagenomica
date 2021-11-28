c8b9ec013bdfcbbbf578d04e228ae0aaef3bb7ef
Felipe Goulart <ofelipegoulart@gmail.com>
2021-11-28 16:48:09 -0300

    auth update

--- src/api.js
+++ src/api.js
@@ -1,20 +1,20 @@
 import axios from "axios";
-require('dotenv').config()
 
-const baseURL = process.env.REACT_APP_API_URL;
+const axiosJWT = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+})
 
-axios.interceptors.request.use(function (config) {
-    const token = store.getState().session.token;
-    config.headers.Authorization =  token;
-
-    return config;
-});
-
-const api = axios.create({
-  baseURL: baseURL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export default api;
+axiosJWT.interceptors.request.use(
+  async (config) => {
+    let currentDate = new Date();
+    const decodedToken = jwt_decode(user.accessToken);
+    if (decodedToken.exp * 1000 < currentDate.getTime()) {
+      data = await refreshToken();
+      config.headers["authorization"] = "Bearer " + data.accessToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
