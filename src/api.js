cf32da73333699f92fc4f5119d2454a216940eb3
Felipe Goulart <ofelipegoulart@gmail.com>
2021-11-28 23:55:25 -0300

    node-env-run commit

--- src/api.js
+++ src/api.js
@@ -3,5 +3,7 @@ require("dotenv").config();
 const process = require('process');
 
 const axiosJWT = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "https://vigilanciagenomica-backend.herokuapp.com/api"
 })
+
+export default axiosJWT;
