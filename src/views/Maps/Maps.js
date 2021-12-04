ee4e5c549e469419ef20d16ee183ba7fa8a9daeb
Felipe Goulart <ofelipegoulart@gmail.com>
2021-12-03 23:51:30 -0300

    map marker update

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -20,17 +20,17 @@ export default function Maps() {
           </Card>
         </GridItem>
       </GridContainer>
-    <MapContainer center={position} zoom={8} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={position}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+      <MapContainer center={position} zoom={7} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </div>
   );
 };
