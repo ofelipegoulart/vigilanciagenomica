a5c56713146e65e8e15bf3e7b9ef35f747f956d6
Felipe Goulart <ofelipegoulart@gmail.com>
2021-12-03 23:57:52 -0300

    map page updated

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -26,9 +26,6 @@ export default function Maps() {
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
     </Marker>
   </MapContainer>
     </div>
