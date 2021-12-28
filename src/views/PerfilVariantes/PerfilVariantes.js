5af2b92e4102ac0e9c47f96e5e04d73ff551f3b0
Felipe Goulart <ofelipegoulart@gmail.com>
2021-12-28 15:27:02 -0300

    legend pie chart

--- src/views/PerfilVariantes/PerfilVariantes.js
+++ src/views/PerfilVariantes/PerfilVariantes.js
@@ -32,6 +32,18 @@ export default function PerfilVariantes() {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+);
 }
 
   return (
