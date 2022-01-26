395da2fbd19273d680153e78d2cadc243c7ecead
Felipe Goulart <ofelipegoulart@gmail.com>
2022-01-25 22:16:16 -0300

    table reports update

--- src/views/Relatorios/relatorios.js
+++ src/views/Relatorios/relatorios.js
@@ -18,7 +18,8 @@ import SE49 from "reports/2021_SE49_Report_Semanal_assinado.pdf";
 import SE50 from "reports/2021_SE50_Report_Semanal_assinado.pdf";
 import SE51 from "reports/2021_SE50_Report_Semanal_assinado.pdf";
 import SE52 from "reports/2021_SE52_Report_Semanal_assinado.pdf";
-import { Table } from '@material-ui/core';
+
+import Table from "components/Table/Table.js";
 import styles from "assets/jss/material-dashboard-react/views/aboutStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -26,96 +27,35 @@ const useStyles = makeStyles(styles);
 export default function Icons() {
   const classes = useStyles();
   return (
+    <div>
     <GridContainer>
       <GridItem xs={12} sm={12} md={12}>
         <Card plain>
           <CardHeader plain color="info">
             <h1>Relatórios Técnicos</h1>
           </CardHeader>
-          <CardBody style={{marginTop:"30px",marginLeft:"auto",marginRight:"auto"}}>
-          <table>
-            <tr>
-              <th>Ano</th>
-              <th>Mês</th>
-              <th>Semana Epidemiológica</th>
-              <th>Link</th>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Dezembro</td>
-              <td>52</td>
-              <td><a href={SE52} target="_blank"> Semana Epidemiológica 52 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Dezembro</td>
-              <td>51</td>
-              <td><a href="#" target="_blank"> Semana Epidemiológica 51 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Dezembro</td>
-              <td>50</td>
-              <td><a href={SE50} target="_blank"> Semana Epidemiológica 50 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Dezembro</td>
-              <td>49</td>
-              <td><a href={SE49} target="_blank"> Semana Epidemiológica 49 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Novembro</td>
-              <td>48</td>
-              <td><a href={SE48} target="_blank"> Semana Epidemiológica 48 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Novembro</td>
-              <td>47</td>
-              <td><a href={SE47} target="_blank"> Semana Epidemiológica 47 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Novembro</td>
-              <td>46</td>
-              <td><a href={SE46} target="_blank"> Semana Epidemiológica 46 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Novembro</td>
-              <td>45</td>
-              <td><a href={SE45} target="_blank"> Semana Epidemiológica 45 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Outubro</td>
-              <td>44</td>
-              <td><a href="#" target="_blank"> Semana Epidemiológica 44 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Outubro</td>
-              <td>43</td>
-              <td><a href="#" target="_blank"> Semana Epidemiológica 43 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Outubro</td>
-              <td>42</td>
-              <td><a href="#" target="_blank"> Semana Epidemiológica 42 [em PDF] </a></td>
-            </tr>
-            <tr>
-              <td>2021</td>
-              <td>Outubro</td>
-              <td>41</td>
-              <td><a href={SE41} target="_blank"> Semana Epidemiológica 41 [em PDF] </a></td>
-            </tr>
-          </table>
-        </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
+          <CardBody style={{marginTop:"30px"}}>
+          <Table
+              tableHeaderColor="info"
+              tableHead={["Ano", "Mês", "Relatório da Semana Epidemiológica"]}
+              tableData={[
+                ["2021", "Dezembro", <a href={SE52} target="_blank"> Semana Epidemiológica 52 [em PDF] </a>],
+                ["2021", "Dezembro", <a href="#" target="_blank"> Semana Epidemiológica 51 [em PDF] </a>],
+                ["2021", "Dezembro", <a href={SE50} target="_blank"> Semana Epidemiológica 50 [em PDF] </a>],
+                ["2021", "Dezembro", <a href={SE49} target="_blank"> Semana Epidemiológica 49 [em PDF] </a>],
+                ["2021", "Novembro", <a href={SE48} target="_blank"> Semana Epidemiológica 48 [em PDF] </a>],
+                ["2021", "Novembro", <a href={SE47} target="_blank"> Semana Epidemiológica 47 [em PDF] </a>],
+                ["2021", "Novembro", <a href={SE46} target="_blank"> Semana Epidemiológica 46 [em PDF] </a>],
+                ["2021", "Novembro", <a href={SE45} target="_blank"> Semana Epidemiológica 45 [em PDF] </a>],
+                ["2021", "Outubro", <a href="#" target="_blank"> Semana Epidemiológica 44 [em PDF] </a>],
+                ["2021", "Outubro", <a href="#" target="_blank"> Semana Epidemiológica 43 [em PDF] </a>],
+                ["2021", "Outubro", <a href="#" target="_blank"> Semana Epidemiológica 42 [em PDF] </a>],
+                ["2021", "Outubro", <a href={SE41} target="_blank"> Semana Epidemiológica 41 [em PDF] </a>],
+              ]}
+            />
+            </CardBody>
+            </Card>
+            </GridItem></GridContainer>
+            </div>
   );
 }
